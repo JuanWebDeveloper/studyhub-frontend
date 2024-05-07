bf83167b180636a4e607dba37c01000f779908cb
Juan Vargas <juandeveloper19@gmail.com>
2024-05-07 06:40:21 -0500

    Created the response model for the 'connection-status' API endpoint

--- src/common/models/index.ts
+++ src/common/models/index.ts
@@ -1,2 +1,4 @@
 //*> Exports of the global state slices.
 export type { UiSlicesModel } from './store/slices/uiSlicesModel';
+//*> Export of the API endpoints responses models.
+export type { APIEndponitConnectionStatus } from './apiEnpointsModels/endpointConnectionStatus';
