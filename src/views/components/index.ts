36fff1028db3ce0a861f75fb3de8992f875898b3
Juan Vargas <juandeveloper19@gmail.com>
2024-05-08 22:46:41 -0500

    Created Loading component for handling loading states

--- src/views/components/index.ts
+++ src/views/components/index.ts
@@ -1,2 +1,4 @@
 //*> Export of the home page component.
 export { HomePageWithAPIConnection } from './pages/home/HomePageWithAPIConnection';
+//*> Export of the shared components of the application.
+export { Loading } from './shared/loading/Loading';
