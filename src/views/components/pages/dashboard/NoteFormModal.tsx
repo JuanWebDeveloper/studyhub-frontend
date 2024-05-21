b8d63cc876160caf96ffa3619abe9f841e5b002a
Juan Vargas <juandeveloper19@gmail.com>
2024-05-21 04:57:03 -0500

    Implemented useForm hook in NoteFormModal component
    This commit integrates the useForm custom hook into the NoteFormModal component to manage the form state. This change simplifies the state management in the component and makes the code more maintainable and reusable.

--- src/views/components/pages/dashboard/NoteFormModal.tsx
+++ src/views/components/pages/dashboard/NoteFormModal.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import Select from 'react-select';
+import { ChangeEventHandler } from 'react';
 //*> Import the note categories.
 import { noteCategories, customNoteCategorySelectStyles } from '@/src/common/utils';
+//*> Import the model for the modal state.
 import { stateBooleanModel } from '@/src/common/models';
+//*> Import the hook to manage the form state.
+import { useForm } from '@/src/common/hooks';
 
 type NoteFormModalProps = {
  modalState: stateBooleanModel;
@@ -11,6 +15,17 @@ type NoteFormModalProps = {
 
 export const NoteFormModal = ({ modalState: [isModalOpen, setIsModalOpen] }: NoteFormModalProps) => {
  const handleModalClose = () => setIsModalOpen(false);
+
+ //*> Use the custom hook to manage the form state.
+ const { formValues, handleInputChange, handleSelectChange, resetOrInitialize } = useForm();
+ const { title, content, category } = formValues;
+
+ const handleSubmit: ChangeEventHandler<HTMLFormElement> = (event) => {
+  event.preventDefault();
+  console.log(formValues);
+  resetOrInitialize();
+  handleModalClose();
+ };
 
  return (
   <div className={`note-form ${isModalOpen ? 'fadeIn' : 'fadeOut'}`}>
@@ -21,18 +36,29 @@ export const NoteFormModal = ({ modalState: [isModalOpen, setIsModalOpen] }: Not
    <div className='form-content'>
     <h2>Create a Note</h2>
 
-    <form>
+    <form onSubmit={handleSubmit}>
      <div className='form-group'>
       <label htmlFor='title'>Title</label>
-      <input type='text' id='title' name='title' placeholder='Type a title for your note' />
+      <input type='text' id='title' name='title' placeholder='Type a title for your note' onChange={handleInputChange} value={title} />
      </div>
+
      <div className='form-group'>
       <label htmlFor='content'>Content</label>
-      <textarea id='content' name='content' placeholder='Type the content of your note here'></textarea>
+      <textarea id='content' name='content' placeholder='Type the content of your note here' onChange={handleInputChange} value={content}></textarea>
      </div>
+
      <div className='form-group'>
-      <Select isSearchable={false} placeholder='Select a category for your note' options={noteCategories} styles={customNoteCategorySelectStyles} />
+      <Select
+       isSearchable={false}
+       placeholder='Select a category for your note'
+       options={noteCategories}
+       styles={customNoteCategorySelectStyles}
+       key={category}
+       onChange={handleSelectChange}
+       value={noteCategories.find((option) => option.value === category || (category === '' && option.label === 'Uncategorized'))}
+      />
      </div>
+
      <button type='submit'>Create Note</button>
     </form>
    </div>
