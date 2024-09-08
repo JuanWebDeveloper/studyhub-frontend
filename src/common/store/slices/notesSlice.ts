1aa35678315d18da9c32d627b2bccba4dbd0e1a9
Juan Vargas <juandeveloper19@gmail.com>
2024-09-07 19:03:31 -0500

    Added 'setAddNotes' action to Redux slice for managing notes
    This action initializes the state with existing notes from the database and appends new notes as they are created, avoiding the need for additional database queries.

--- src/common/store/slices/notesSlice.ts
+++ src/common/store/slices/notesSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { NotesSlicesModel } from '@/src/common/models/';
 
 const initialState: NotesSlicesModel[] = [];
@@ -6,7 +6,12 @@ const initialState: NotesSlicesModel[] = [];
 export const notesSlice = createSlice({
  name: 'notes',
  initialState,
- reducers: {},
+ reducers: {
+  setAddNotes: (state, action: PayloadAction<NotesSlicesModel | NotesSlicesModel[]>) => {
+   const notes = Array.isArray(action.payload) ? action.payload : [action.payload];
+   state.push(...notes);
+  },
+ },
 });
 
-export const {} = notesSlice.actions;
+export const { setAddNotes } = notesSlice.actions;
